657b56d1b594ffda6e6356a09446e3c8a7ef583b
Bartolome Marin Sanchez <bartms@gmail.com>
2011-01-25 12:34:53 +0100

    First version

---
+++ src/es/libresoft/openhealth/android/test/client/ManagerTestClient.java
@@ -0,0 +1,13 @@
+package es.libresoft.openhealth.android.test.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ManagerTestClient extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
