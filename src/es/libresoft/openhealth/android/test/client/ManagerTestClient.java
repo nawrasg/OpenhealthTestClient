6e114dbf309efe7899e3e2e706d708cfad4aa0bc
Bartolome Marin Sanchez <bartms@gmail.com>
2011-01-26 09:05:16 +0100

    Launch service when the application starts

--- src/es/libresoft/openhealth/android/test/client/ManagerTestClient.java
+++ src/es/libresoft/openhealth/android/test/client/ManagerTestClient.java
@@ -1,13 +1,31 @@
 package es.libresoft.openhealth.android.test.client;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ManagerTestClient extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	static String serviceName = new String("es.libresoft.openhealth.android.OPENHEALTH_SERVICE");
+
+	public boolean initManager() {
+		try {
+			Intent intentDroid = new Intent(serviceName);
+			startService(intentDroid);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.err.println("Service launched");
+		return true;
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		initManager();
+	}
 }
