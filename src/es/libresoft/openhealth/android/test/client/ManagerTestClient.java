a8306ffaeb9ef37050f5ef42679e9526ebe5cf7e
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-01-26 09:38:52 +0100

    Bind to the service and create if needed

--- src/es/libresoft/openhealth/android/test/client/ManagerTestClient.java
+++ src/es/libresoft/openhealth/android/test/client/ManagerTestClient.java
@@ -1,20 +1,32 @@
 package es.libresoft.openhealth.android.test.client;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class ManagerTestClient extends Activity {
 
-	static String serviceName = new String("es.libresoft.openhealth.android.OPENHEALTH_SERVICE");
+	private static String serviceName = new String("es.libresoft.openhealth.android.OPENHEALTH_SERVICE");
+	private ServiceConnection managerService = new ServiceConnection(){
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			System.err.println("TODO: Service connected " + name);
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			System.err.println("TODO: Service disconnected " + name);
+		}
+	};
 
 	public boolean initManager() {
-		try {
-			Intent intentDroid = new Intent(serviceName);
-			startService(intentDroid);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
+
+		bindService(new Intent(serviceName), managerService, Context.BIND_AUTO_CREATE);
 
 		System.err.println("Service launched");
 		return true;
