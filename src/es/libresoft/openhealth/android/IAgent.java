23bc73dc439e9dd7d1fb6fbfae315374fe37a996
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-01-31 14:59:15 +0100

    Add new IAgent Parcelable object

--- src/es/libresoft/openhealth/android/IAgent.java
+++ src/es/libresoft/openhealth/android/IAgent.java
@@ -1,3 +1,29 @@
+/*
+Copyright (C) 2011 GSyC/LibreSoft, Universidad Rey Juan Carlos.
+
+Author: Jose Antonio Santos Cadenas <jcaden@libresoft.es>
+Author: Santiago Carot Nemesio <scarot@libresoft.es>
+
+This program is a (FLOS) free libre and open source implementation
+of a multiplatform manager device written in java according to the
+ISO/IEEE 11073-20601. Manager application is designed to work in
+DalvikVM over android platform.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package es.libresoft.openhealth.android;
 
 import android.os.Parcel;
@@ -8,16 +34,20 @@ public class IAgent implements Parcelable {
 
 	public static final Parcelable.Creator<IAgent> CREATOR =
 			new Parcelable.Creator<IAgent>() {
-	    public IAgent createFromParcel(Parcel in) {
-	        return new IAgent(in);
-	    }
+		public IAgent createFromParcel(Parcel in) {
+			return new IAgent(in);
+		}
 
-	    public IAgent[] newArray(int size) {
-	        return new IAgent[size];
-	    }
+		public IAgent[] newArray(int size) {
+			return new IAgent[size];
+		}
 	};
 
 	private IAgent (Parcel in) {
+		readFromParcel(in);
+	}
+
+	public void readFromParcel(Parcel in) {
 		id = in.readInt();
 	}
 
@@ -36,10 +66,11 @@ public class IAgent implements Parcelable {
 	}
 
 	public boolean equals(Object o) {
-		if (!(o instanceof IAgent))
-			return false;
+		if (o instanceof IAgent) {
+			IAgent agent = (IAgent) o;
+			return this.id == agent.id;
+		}
 
-		IAgent agent = (IAgent) o;
-		return this.id == agent.id;
+		return false;
 	}
 }
