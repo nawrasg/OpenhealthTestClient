1e6dc4eded5d280b2e29a565e8495cd5b1b554cc
Bartolome Marin Sanchez <bartms@gmail.com>
2011-03-05 11:26:55 +0100

    Add showAttributes in AgentAttributeView. There is a "bug" at first load of the tab becouse the bindservice of IAgentService doesn't finished yet.

--- src/es/libresoft/openhealth/android/test/client/AgentAttributeView.java
+++ src/es/libresoft/openhealth/android/test/client/AgentAttributeView.java
@@ -26,15 +26,139 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package es.libresoft.openhealth.android.test.client;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 import es.libresoft.openhealth.android.aidl.IAgent;
+import es.libresoft.openhealth.android.aidl.IAgentService;
+import es.libresoft.openhealth.android.aidl.types.IAttribute;
+import es.libresoft.openhealth.android.aidl.types.IError;
+import es.libresoft.openhealth.android.aidl.types.objects.IMDS;
 
 public class AgentAttributeView extends Activity {
 
 	private IAgent agent = null;
+
+	private void show(String msg) {
+		Toast t = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+		t.setGravity(Gravity.CENTER, 0, 0);
+		t.show();
+	}
+
+	private boolean getAttributes(ArrayList<IAttribute> attrs) {
+		IError err = new IError();
+		try {
+			IAgentService agentService = (IAgentService)
+				GlobalStorage.getInstance().get(IAgentService.class.toString());
+			if (agentService == null) {
+				show("Error getting attributes:\nAgentService not exist");
+				return false;
+			}
+
+			IMDS mds = new IMDS();
+			agentService.getMDS(agent, mds, err);
+			if (err.getErrCode() != 0) {
+				show("Error getting MDS " + err.getErrMsg());
+				System.err.println("Error getting MDS " + err.getErrMsg());
+				return false;
+			}
+
+			agentService.getObjectAttrs(mds, attrs, err);
+			if (err.getErrCode() != 0) {
+				show("Error getting attributes " + err.getErrMsg());
+				System.err.println("Error getting attributes " + err.getErrMsg());
+			}
+		} catch (RemoteException e) {
+			show("RemoteException in agentService.getAttributes");
+			System.err.println("RemoteException in agentService.getAttributes" + e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+
+		return err.getErrCode() == 0;
+	}
+
+	private void showAttributes() {
+		ArrayList<IAttribute> attrs = new ArrayList<IAttribute>();
+
+		if (!getAttributes(attrs)) return;
+
+		TableLayout tl = (TableLayout)findViewById(R.id.agentattributetable);
+		tl.removeAllViews();
+		TableRow tr = null;
+		TextView tvname = null;
+		TextView tvvalue = null;
+		for (IAttribute attr : attrs) {
+			tr = new TableRow(this);
+			tvname = new TextView(this);
+			tvvalue = new TextView(this);
+			tvname.setText(attr.getAttrIdStr());
+			tvvalue.setText(attr.getAttr().toString());
+			tr.addView(tvname);
+			tr.addView(tvvalue);
+			tl.addView(tr);
+		}
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.agentattributeview);
+
+		Bundle extras  = getIntent().getExtras();
+		if (extras == null || !extras.containsKey("agent")) {
+			show("Not sended agent");
+			finish();
+			return;
+		}
+		agent = extras.getParcelable("agent");
+		showAttributes();
+		//TODO: There is a "bug" for first showAttributes because agentService is'nt registered yet
+		// add a dialog and asynctask waiting for the services of AgentView (IAgentService, IPMStoreService) are binded
+		// as Dialog Notification from http://developer.android.com/guide/topics/ui/notifiers/index.html
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.agentattributeviewmenu, menu);
+	    return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected (MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.MENU_ATTRIBUTES:
+				showAttributes();
+				break;
+			case R.id.MENU_DISCONNECT:
+				IAgentService agentService = (IAgentService)
+					GlobalStorage.getInstance().get(IAgentService.class.toString());
+				if (agentService != null) {
+					try {
+						IError error = new IError();
+						if (!agentService.disconnect(agent, error)) {
+							show("Error disconnecting: " + error.getErrMsg());
+						}
+					} catch (RemoteException e) {
+						show("Can't connect to the remote Service");
+					}
+				}
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 /*	
 	private IManagerClientCallback msc = new IManagerClientCallback.Stub() {
 
@@ -93,13 +217,13 @@ public class AgentAttributeView extends Activity {
 			return;
 		}
 	}
-*/
+
 	private void show(String msg) {
 		Toast t = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
 		t.setGravity(Gravity.CENTER, 0, 0);
 		t.show();
 	}
-/*
+
 	private boolean getAttributes(ArrayList<IAttribute> attrs) {
 		IError err = new IError();
 		try {
@@ -151,7 +275,7 @@ public class AgentAttributeView extends Activity {
 			tl.addView(tr);
 		}
 	}
-*/
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -165,15 +289,16 @@ public class AgentAttributeView extends Activity {
 			return;
 		}
 		agent = extras.getParcelable("agent");
-//		showAttributes();
+		showAttributes();
 
-//		registerManagerCallbacks();
+		registerManagerCallbacks();
 	}
 
 	@Override
 	protected void onDestroy() {
-//		unregisterManagerCallbacks();
+		unregisterManagerCallbacks();
 
 		super.onDestroy();
 	}
+*/
 }
